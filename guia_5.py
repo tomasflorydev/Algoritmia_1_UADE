b33fc4331154ca6ea1cc01029219c8cb92124bcd
Tomas Flory <tomsflory@192.168.1.6>
2025-05-12 10:39:57 -0300

    ejercicios2,3

--- guia_5.py
+++ guia_5.py
@@ -11,18 +11,83 @@ lista_tupla[0] = dia
 new_tupla_fecha = tuple(lista_tupla)
 print(new_tupla_fecha) """
 
-horario = input("Ingrese una hora")
+""" hora = int(input("Ingrese la hora"))
+while hora > 23 or hora < 0:
+    hora = int(input("Ingrese una hora valida"))
+minutos = int(input("Ingrese los minutos"))
+while minutos > 59 or minutos < 0:
+    minutos = int(input("Ingrese una cantidad de minutos valida"))
+tupla = (hora, minutos)
+hora_n2 = int(input("Ingrese la hora"))
+while hora_n2 > 23 or hora_n2 < 0:
+    hora_n2 = int(input("Ingrese una hora valida"))
+minutos_n2 = int(input("Ingrese los minutos"))
+while minutos_n2 > 59 or minutos_n2 < 0:
+    minutos_n2 = int(input("Ingrese una cantidad de minutos valida"))
+tupla_n2 = (hora_n2, minutos_n2) """
+"""
+En esta primera parte se crean las tuplas y 
+se hace la verificacion de valores
+"""
+""" minutos_totales = hora * 60 + minutos
+minutos_totales_n2 = hora_n2 * 60 + minutos_n2
 
-horario_tupla = (horario.split(":"))
-lista_tupla = list(horario_tupla)
+if minutos_totales > minutos_totales_n2:
+    minutos_totales_n2 = minutos_totales_n2 + 1440 # Se suma 1440 ya que es la cantidad de minutos en un dia
+    diferencia_minutos = minutos_totales_n2 - minutos_totales
+    print(f"La diferencia es de {diferencia_minutos} minutos.")
 
-for i in lista_tupla:
-    if lista_tupla[0] > 24:
-        print("Error horario invalido")
-        break
-    
-    elif lista_tupla[1] > 59:
-        print("Error horario invalido")
-        break
+
+elif minutos_totales_n2 > minutos_totales:
+    minutos_totales = minutos_totales  + 1440
+    diferencia_minutos = minutos_totales - minutos_totales_n2
+    print(f"La diferencia es de {diferencia_minutos} minutos.")
+
+
+else: 
+    print("Ambos horarios corresponden al mismo momemnto")
+ """
+
+# Ejercico 2
+""" nombres_meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+def pedir_fecha():
+    dia = int(input("Ingrese el día: "))
+    mes = int(input("Ingrese el mes: "))
+    año = int(input("Ingrese el año: "))
+    tupla = (dia, mes, año)  # Guardamos la tupla en una variable llamada 'tupla'
+    return tupla
+
+def fecha_en_texto(tupla):
+    dia, mes, año = tupla  # Desempaquetamos la tupla en variables
+    corte = 30  # Año de corte
+
+    # Corregir el año si es de dos dígitos
+    if año < 100:
+        if año <= corte:
+            año += 2000
+        else:
+            año += 1900
+
+    # Armar y devolver el string con f-string
+    fecha_texto = f"{dia} de {nombres_meses[mes - 1]} de {año}"
+    print(fecha_texto)
+
+fecha = pedir_fecha()  
+fecha_en_texto(fecha)  
+ """
+
+#Ejercicio 3
+""" def pedir_correo():
+    correo = input("Ingrese su direccion de correo electronico")
+    if "@" not in correo:
+        tupla_correo = ()
+        return tupla_correo
     else:
-        print("Horario valido")
+        usuario_dominio = correo.split("@")  
+        dominio_extension = usuario_dominio[1].split(".")
+        tupla_correo = (usuario_dominio[0], dominio_extension[0], dominio_extension[1])
+        print(tupla_correo)
+
+
+pedir_correo() """
